36457e1c8dab8f6875bf8f4f1864a0a1af978546
Paweł Kózka <pawel.k.h1995@gmail.com>
2020-02-27 21:27:50 +0100

    first commit

---
+++ src/main/java/hotelService/Main.java
@@ -0,0 +1,12 @@
+package hotelService;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Launcher launcher = new Launcher();
+        launcher.startProgram();
+
+
+    }
+}
