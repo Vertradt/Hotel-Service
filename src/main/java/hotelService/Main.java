6aa66d03635fa4b3e9cba10a91a3b109dc4f21aa
Paweł Kózka <pawel.k.h1995@gmail.com>
2020-02-29 23:00:42 +0100

    Created repository of room

--- src/main/java/hotelService/Main.java
+++ src/main/java/hotelService/Main.java
@@ -1,9 +1,22 @@
 package hotelService;
-
+//        1. Utwórz nowe repozytorium HotelService a następnie wypchnij na nie pusty projekt (maven
+//        project z archetypu maven-archetype-quickstart). Pamiętaj o zignorowaniu niepotrzebnych
+//        plików.
+//        2. Utwórz gałąź develop.
+//        3. Wszystkie nowe funkcjonalności wprowadzaj na nowych gałęziach (najlepiej feature).
+//        4. Dodaj klasę Room reprezentującą pojedynczy pokój (nr pokoju, ilu osobowy, czy w pokoju jest
+//        łazienka - true/false oraz czy pokój jest dostępny - true/false).
+//        5. Dodaj klasę Hotel która będzie zawierała listę pokoi.
+//        6. W konstruktorze klasy Hotel utwórz kilka obiektów klasy Room i dodaj je do listy (np. 10-15),
+//        dzięki temu podczas tworzenia instancji obiektu hotel, automatycznie generowana będzie lista
+//        pokoi.
+//
 
 public class Main {
 
     public static void main(String[] args) {
+        Launcher launcher = new Launcher();
+        launcher.startProgram();
 
 
     }
---
+++ src/main/java/hotelService/UserService.java
@@ -0,0 +1,16 @@
+package hotelService;
+//    7. Dodaj klasę UserService która będzie służyć do obsługi hotelu. Na początek dodaj
+//        funkcjonalności:
+//        1. Pobierz listę wszystkich pokoi.
+//        2. Pobierz listę wszystkich dostępnych pokoi.
+//        3. Rezerwuj pokój (podaj nr pokoju i jeśli jest dostępny to go zarezerwuj).
+//        4. Zwolnij pokój (podaj nr pokoju i jeśli jest zajęty to go zwolnij).
+public class UserService {
+    private Hotel hotel;
+
+    public UserService(Hotel hotel) {
+        this.hotel = hotel;
+    }
+
+
+}
