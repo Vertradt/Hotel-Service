25382dbb5edb71eeaf0df6fc6f557d8beb207c1c
Paweł Kózka <pawel.k.h1995@gmail.com>
2020-03-14 19:18:24 +0100

    Code refactored. A lot of changes has been provided

--- src/main/java/hotelService/TooBigNumberException.java
+++
@@ -1,9 +0,0 @@
-package hotelService;
-
-public class TooBigNumberException extends RuntimeException {
-    //TODO tu też
-    public TooBigNumberException() {
-        System.out.println("Za duża liczba");
-
-    }
-}
